ccdebdaaf4b9f642c54f8835d4b83b2909cf4549
Nataliia-Homyk324 <homyk324@gmail.com>
2024-06-08 11:07:18 +0300

    post

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,4 +1,4 @@
-import { getAllContacts, getContactById } from '../services/contacts.js';
+import { getAllContacts, getContactById, createContact } from '../services/contacts.js';
 import createHttpError from 'http-errors';
 
 
@@ -35,5 +35,23 @@ export const getContactByIdController = async (req, res, next) => {
     status: 200,
     message: `Successfully found contact with id ${contactId}!`,
     data: contact,
+  });
+};
+
+export const createContactController = async (req, res, next) => {
+
+  const { name, phoneNumber, email, isFavourite, contactType } = req.body;
+
+  if (!name || !phoneNumber) {
+     next(createHttpError(400, 'Name and phoneNumber are required'));
+    return;
+  }
+
+   await createContact(req.body);
+
+  res.status(201).json({
+    status: 201,
+    message: `Successfully created a contact!`,
+    data: { name, phoneNumber, email, isFavourite, contactType },
   });
 };
