76d6f4a8758c2e64b91da9437f600664288e6634
Nataliia-Homyk324 <homyk324@gmail.com>
2024-06-13 08:01:05 +0300

    isValidId

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -8,6 +8,7 @@ import {
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middlewares/validateBody.js';
+import { isValidId } from '../middlewares/isValidId.js';
 import { createContactSchema, updateContactSchema } from '../validation/contacts.js';
 
 
@@ -15,12 +16,13 @@ const router = Router();
 
 router.get('/contacts', ctrlWrapper(getContactsController));
 
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+router.get('/contacts/:contactId',isValidId, ctrlWrapper(getContactByIdController));
 
 router.post('/contacts',validateBody(createContactSchema), ctrlWrapper(createContactController));
 
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
+router.delete('/contacts/:contactId',isValidId, ctrlWrapper(deleteContactController));
 
-router.patch('/contacts/:contactId',validateBody(updateContactSchema), ctrlWrapper(patchContactController));
+router.patch('/contacts/:contactId', isValidId, validateBody(updateContactSchema), ctrlWrapper(patchContactController));
+
 
 export default router;
