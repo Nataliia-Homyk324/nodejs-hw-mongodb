e3c8b1f94de345b4015f4b318e6590a7c77e0d4a
Nataliia-Homyk324 <homyk324@gmail.com>
2024-06-15 19:18:33 +0300

    Token

--- src/index.js
+++ src/index.js
@@ -11,4 +11,8 @@ export const SORT_ORDER = {
   DESC: 'desc',
 };
 
+export const FIFTEEN_MINUTES = 15 * 60 * 1000;
+export const THIRTY_DAY = 24 * 30 * 60 * 60 * 1000;
+
 bootstrap();
+//thirty
--- src/routers/auth.js
+++ src/routers/auth.js
@@ -3,6 +3,8 @@ import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { registerUserSchema } from '../validation/auth.js';
 import { registerUserController } from '../controllers/auth.js';
 import { validateBody } from '../middlewares/validateBody.js';
+import { loginUserSchema} from '../validation/auth.js';
+import { loginUserController } from '../controllers/auth.js';
 
 const authRouter = Router();
 
@@ -10,6 +12,12 @@ authRouter.post(
   '/register',
   validateBody(registerUserSchema),
   ctrlWrapper(registerUserController),
+);
+
+authRouter.post(
+  '/login',
+  validateBody(loginUserSchema),
+  ctrlWrapper(loginUserController),
 );
 
 export default authRouter;
