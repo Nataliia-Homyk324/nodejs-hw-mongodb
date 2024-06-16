4f7b41d10d55fe65e45026fdb1294fdb79f7d641
Nataliia-Homyk324 <homyk324@gmail.com>
2024-06-16 11:54:41 +0300

    step 8

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -24,7 +24,7 @@ export const getContactsController = async (
 
 	  res.json({
 	    status: 200,
-	    message: 'Successfully found students!',
+	    message: 'Successfully found contacts!',
 	    data: contacts,
 	  });
 	} catch(err) {
@@ -96,3 +96,95 @@ export const patchContactController = async (req, res, next) => {
     data: result.contact,
   });
 };
+
+// з userID
+// export const getContactsController = async (
+//   userId, req,
+//   res,
+// 	next,
+// ) => {
+//   try {
+//     const { page, perPage } = parsePaginationParams(req.query);
+//     const { sortBy, sortOrder } = parseSortParams(req.query);
+//     const filter = parseFilterParams(req.query);
+
+// 	  const contacts = await getAllContacts({
+//      userId, page,
+//      perPage,
+//      sortBy,
+//      sortOrder,
+//      filter,
+//    });
+
+// 	  res.json({
+// 	    status: 200,
+// 	    message: 'Successfully found contacts!',
+// 	    data: contacts,
+// 	  });
+// 	} catch(err) {
+// 		next(err);
+// 	}
+// };
+
+// export const getContactByIdController = async (req, res, next) => {
+//   const { contactId,userId  } = req.params;
+//   const contact = await getContactById(contactId, userId);
+
+//   if (!contact) {
+//     // 2. Створюємо та налаштовуємо помилку
+//     next(createHttpError(404, 'Contact not found'));
+//     return;
+//   }
+
+//   res.json({
+//     status: 200,
+//     message: `Successfully found contact with id ${contactId}!`,
+//     data: contact,
+//   });
+// };
+
+// export const createContactController = async (req, res, next) => {
+//   const { name, phoneNumber } = req.body;
+
+//   if (!name || !phoneNumber) {
+//     next(createHttpError(400, 'Name and phoneNumber are required'));
+//     return;
+//   }
+//   delete req.body._V;
+//   const newContact = await createContact(req.body);
+
+//   res.status(201).json({
+//     status: 201,
+//     message: `Successfully created a contact!`,
+//     data: newContact,
+//   });
+// };
+
+// export const deleteContactController = async (req, res, next) => {
+//   const { contactId,userId } = req.params;
+
+//   const contact = await deleteContact(contactId,userId);
+
+//   if (!contact) {
+//     next(createHttpError(404, 'Contact not found', { message: 'Contact not found' }));
+//     return;
+//   }
+
+//   res.status(204).send();
+// };
+
+// export const patchContactController = async (req, res, next) => {
+//   const { contactId,userId } = req.params;
+//   const result = await updateContact(contactId,userId, req.body);
+
+//   if (!result) {
+//     next(createHttpError(404, 'Contact not found',   { message: 'Contact not found' } ));
+//     return;
+//   }
+
+//   res.json({
+//     status: 200,
+//     message: `Successfully patched a contact!`,
+//     data: result.contact,
+//   });
+// };
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -30,3 +30,37 @@ contactsRouter.patch('/:contactId', isValidId, validateBody(updateContactSchema)
 
 
 export default contactsRouter;
+
+
+//оновлений роутер
+
+
+// import { Router } from 'express';
+// import {
+//   getContactsController,
+//   getContactByIdController,
+//   createContactController,
+//   deleteContactController,
+//   patchContactController,
+// } from '../controllers/contacts.js';
+// import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+// import { validateBody } from '../middlewares/validateBody.js';
+// import { isValidId } from '../middlewares/isValidId.js';
+// import { createContactSchema, updateContactSchema } from '../validation/contacts.js';
+// import { authenticate } from '../middlewares/authenticate.js';
+
+// const contactsRouter = Router();
+
+// contactsRouter.use(authenticate);
+
+// contactsRouter.get('/', ctrlWrapper((req, res) => getContactsController({ ...req.query, userId: req.user._id })));
+
+// contactsRouter.get('/:contactId', isValidId, ctrlWrapper((req, res) => getContactByIdController({ contactId: req.params.contactId, userId: req.user._id })));
+
+// contactsRouter.post('/', validateBody(createContactSchema), ctrlWrapper((req, res) => createContactController({ ...req.body, userId: req.user._id })));
+
+// contactsRouter.delete('/:contactId', isValidId, ctrlWrapper((req, res) => deleteContactController({ contactId: req.params.contactId, userId: req.user._id })));
+
+// contactsRouter.patch('/:contactId', isValidId, validateBody(updateContactSchema), ctrlWrapper((req, res) => patchContactController({ contactId: req.params.contactId, userId: req.user._id, ...req.body })));
+
+// export default contactsRouter;
